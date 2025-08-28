78979f0fe3269c8559b35a206a171476f7900eb0
the truth <shiftrams@gmail.com>
2025-08-28 20:57:19 +0300

    refactor: add product models

--- src/app/core/schemas/products.py
+++ src/app/core/schemas/products.py
@@ -1,9 +1,18 @@
 from typing import Optional
+from datetime import datetime
 from pydantic import BaseModel, Field
 
 from .etc import PyObjectId
 
-class ProductItem(BaseModel):
+class ProductBase(BaseModel):
+  brand: str
+  title: str
+  description: str
+  price: int
+
+class ProductItem(ProductBase):
   id: Optional[PyObjectId] = Field(alias="_id", default=None)
-  name: str
-  date: str
+  category: str
+
+class ProductCreate(ProductBase):
+  date: datetime
