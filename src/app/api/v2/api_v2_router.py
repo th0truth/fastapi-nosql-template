3f678032a07ddff9f5ad90d6aefdb015c29e760d
the truth <shiftrams@gmail.com>
2025-08-28 17:37:29 +0300

    feat: change items route to products

--- src/app/api/v2/api_v2_router.py
+++ src/app/api/v2/api_v2_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from .routers import (
-  items
+  products
 )
 
 # Initilize v2 router
 api_v2_router = APIRouter()
 
 # Include routers
-api_v2_router.include_router(items.router, prefix="/items")
+api_v2_router.include_router(products.router, prefix="/products")
