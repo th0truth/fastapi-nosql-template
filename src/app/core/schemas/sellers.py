90e46ad9bdc8b36a3587b70ec587a87708429309
the truth <shiftrams@gmail.com>
2025-08-28 20:45:27 +0300

    feat(api): add seller base model

---
+++ src/app/core/schemas/sellers.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from .user import UserPrivate
+
+class SellerBase(UserPrivate):
+  role: str = "sellers"
+  scopes: list = [
+    "seller"
+  ]
+  business_email: Optional[str] = None
+  business_address: Optional[str] = None
+  identity_card: str
+  business_name: str
+  storefront_name: str
+  address: str
