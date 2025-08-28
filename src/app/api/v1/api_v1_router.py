43162edbf3d14b4012df11df4c2b30edd69a041a
the truth <shiftrams@gmail.com>
2025-08-28 20:44:26 +0300

    feat(api): add sellers route

--- src/app/api/v1/api_v1_router.py
+++ src/app/api/v1/api_v1_router.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter
 from .routers import (
   auth,
   user,
-  users
+  users,
+  sellers
 )
 
 # Initilize v1 router
@@ -12,3 +13,4 @@ api_v1_router = APIRouter()
 api_v1_router.include_router(auth.router, prefix="/auth")
 api_v1_router.include_router(user.router, prefix="/user")
 api_v1_router.include_router(users.router, prefix="/users")
+api_v1_router.include_router(sellers.router, prefix="/sellers")
