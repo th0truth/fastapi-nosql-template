9203834bc31e57b711c1c7b7578d1b40b4c5654d
th0truth <shiftrams@gmail.com>
2025-10-24 20:44:25 +0300

    feat: add class DBConnection as abstract base class

--- src/app/core/security/utils.py
+++ src/app/core/security/utils.py
@@ -1,4 +1,5 @@
 from passlib.context import CryptContext
+from abc import ABC, abstractmethod
 
 class Hash:
   context = CryptContext(schemes=["argon2"], deprecated="auto")
@@ -12,3 +13,19 @@ class Hash:
   def verify(cls, plain: str, hashed: str) -> bool:
     """Return bool type of the verified password."""
     return cls.context.verify(secret=plain, hash=hashed)
+  
+ 
+class DBConnection(ABC):
+  """Abstract base class for DB instance client."""
+
+  @classmethod
+  @abstractmethod
+  async def connect(cls):
+    """Establish instance connection."""
+    pass
+
+  @classmethod
+  @abstractmethod
+  async def close(cls):
+    """Close instance connection."""
+    pass
