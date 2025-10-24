911762610a7fa961879dafa48c0ce947c9f6b2ed
th0truth <shiftrams@gmail.com>
2025-10-24 20:49:43 +0300

    feat: add method that create a venv

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,25 +3,24 @@
 # Exit in case of error
 set -e
 
-# Create venv
-
-if [ ! -d ".venv" ]; then
-  echo "Creating venv..."
-  python -m venv .venv
-fi
-
-# Activate the venv 
+# Check OS
 if [[ "$(uname -s)" == "Linux" ]]; then
-	source .venv/bin/activate
+  python3 -m venv .venv
+  source .venv/bin/activate
 
 elif [[ "$(uname -s)" == "Darwin" ]]; then
-	source ./venv/bin/activate
+  python3 -m pip install virtualenv
+  python3 -m virtualenv .venv
+  source ./venv/bin/activate
 
 elif [[ "$(uname -s)" == "CYGWIN"  || "$(uname -s)" == "MINGW"* ]]; then
-	source .venv\Scripts\activate
+  python -m venv .venv
+  .venv\Scripts\activate
+
 else
-	echo "Unknown OS"
+  echo "Unknown OS"
 fi
+
 
 # Upgrade pip inside venv
 pip install --upgrade pip
