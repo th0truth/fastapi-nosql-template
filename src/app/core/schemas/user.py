9e222c8cfa627360cd460c853903da9ce7ac6314
th0truth <shiftrams@gmail.com>
2025-11-18 17:39:33 +0200

    feat(api): add password length limit

--- src/app/core/schemas/user.py
+++ src/app/core/schemas/user.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from datetime import datetime
 from pydantic import BaseModel, EmailStr
+from .etc import PASSWORDstr
 
 class UserInitial(BaseModel):
   first_name: str
@@ -16,7 +17,7 @@ class UserBase(UserInitial):
 class UserPrivate(UserBase):
   account_date: datetime
   email: Optional[EmailStr] = None
-  password: Optional[str] = None
+  password: Optional[PASSWORDstr] = None
   scopes: list
 
 class UserUpdate(BaseModel):
